581f22c0e6b81d88ff520fc2f8055473048a6fa3
Airene Maria <aireneapacible@gmail.com>
2021-10-18 21:13:16 +0800

    Removes task description req

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :category, :optional => true
-  validates :title, presence: true, length: { maximum: 15 }
-  validates :body, presence: true, length: { minimum: 5 }
+  validates :title, presence: true, length: { maximum: 20 }
   validates :category_id, presence: true
   validates :date, presence: true
 end
